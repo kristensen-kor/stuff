3eccbd8f6838aaa0f45dd446a361a33698d5ae0f
Kristensen <kristensen666@gmail.com>
2013-06-17 00:06:36 +0300

    google code jam

---
+++ b_reverse_words.d
@@ -0,0 +1,19 @@
+import std.stdio;
+import std.array;
+
+void main() {
+	auto t = File("B-large-practice.in", "r");
+	auto to = File("out.txt", "w");
+	uint cnt;
+	t.readf(" %d ", &cnt);
+
+	string str;
+
+	for (uint i = 0; i < cnt; i++) {
+		t.readln(str);
+		to.writef("Case #%d:", i + 1);
+		foreach (s; split(str).reverse)
+			to.writef(" %s", s);
+		to.writeln;
+	}
+}
